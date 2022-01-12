b5e11841e70449c0104b42c96f066873b749e60a
jcpcabanada <jcpcabanada@gmail.com>
2022-01-11 21:00:39 -0800

    9th

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
         if (!post) {
@@ -36,13 +36,13 @@ router.get('/:id', async(req, res) => {
 })
 
 router.post('/', (req, res) => {
-    const { title, contents} = req.body
-    if (!title||!contents) {
+    const {title, contents} = req.body
+    if (!title || !contents) {
         res.status(400).json({
             message: "Please provide title and contents for the post"
         })
-    }else {
-        Post.insert({ title, contents})
+    } else {
+        Post.insert({title, contents})
             .then(({id}) => {
                 return Post.findById(id)
             })
@@ -59,7 +59,7 @@ router.post('/', (req, res) => {
     }
 })
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const datPost = await Post.findById(req.params.id)
         if (!datPost) {
@@ -80,8 +80,42 @@ router.delete('/:id', async(req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-
+    const {title, contents} = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist",
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
 })
+
 
 router.get('/:id/comments', (req, res) => {
 
