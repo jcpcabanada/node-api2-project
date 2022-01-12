bd50bd219cc3f81fd59fddc62d3a795a51237267
jcpcabanada <jcpcabanada@gmail.com>
2022-01-11 18:06:47 -0800

    5th stupid period in server.js now works test

--- api/server.js
+++ api/server.js
@@ -5,9 +5,9 @@ const server = express()
 
 server.use(express.json())
 
-server.use('./api/posts', postsRouter)
+server.use('/api/posts', postsRouter)
 
-server.use("*", (req, res) => {
+server.use('*', (req, res) => {
     res.status(404).json({
         message:'Not Found'
     })
